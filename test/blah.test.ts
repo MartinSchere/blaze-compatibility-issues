e92425edd37f7d78af994988bc8a5138a676fb17
Micah Kendall <micahk.accounts@icloud.com>
2024-07-12 01:26:45 +1000

    fix: jest works

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -4,7 +4,8 @@ import { Maestro } from '@blaze-cardano/sdk';
 describe('CIP30Wallet', () => {
   it('works', async () => {
     const wallet = await CIP30Wallet.init(
-      '06facaa87ff0b958370e3eb98db297511f80c30b2488fa96a8e8b4a1119565c6',
+      // I extended the key (64 -> 192) by concatenating it with itself a couple times
+      '06facaa87ff0b958370e3eb98db297511f80c30b2488fa96a8e8b4a1119565c606facaa87ff0b958370e3eb98db297511f80c30b2488fa96a8e8b4a1119565c606facaa87ff0b958370e3eb98db297511f80c30b2488fa96a8e8b4a1119565c6',
       new Maestro({
         apiKey: '',
         network: 'preprod',
